eb387548a630bdaf08704ffb71ebff0cd64fd46e
Hojun Cho <hojun.cho@kaist.ac.kr>
2019-09-27 18:17:25 +0900

    Implemented test code for dataset

--- datasets.py
+++ datasets.py
@@ -3,6 +3,19 @@ from torchvision.datasets import ImageFolder
 import os
 from download_ffhq import run
 
+#---------------------------------------------------------------------
+# Import packages for testing
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+#---------------------------------------------------------------------
+
 class FFHQ(ImageFolder):
     def __init__(self, root='Data', train=True, size='thumbs', stats=False, transform=None, target_transform=None):
         print('Initializing FFHQ dataset: ' + size + (' training' if train else ' validation') + ' data')
@@ -27,3 +40,27 @@ class FFHQ(ImageFolder):
     
     def __getitem__(self, index):
         return super(FFHQ, self).__getitem__(index)[0]
+
+#---------------------------------------------------------------------
+# Test code
+
+if __name__ == "__main__":
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = FFHQ(root='~/Data/Flickr-Face-HQ', train=False, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+
+    dataiter = iter(trainloader)
+    images = dataiter.next()
+
+    # show images
+    img = torchvision.utils.make_grid(images)
+    img = img /2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+#---------------------------------------------------------------------
