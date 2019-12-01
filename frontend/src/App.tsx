986f24ba83bbee4f68f8a4c66fc08a671f48b3c3
Jeff Willette <jrwillette88@gmail.com>
2019-12-01 20:50:03 +0900

    added cropperjs to frontend
    added some config files for typescript
    added declarations file for typeless package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,70 @@
-import React from 'react';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme';
-import { Button, CssBaseline } from '@material-ui/core';
+import React, { useState, useRef } from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import theme from "./theme";
+import Cropper from 'react-cropper';
+import { CssBaseline, AppBar, Toolbar, Typography, Button } from "@material-ui/core";
+import 'cropperjs/dist/cropper.css'
+
+const api = "http://localhost:5000/generate"
+
+const UploadImage: React.FC = () => {
+    const [imgSrc, setImgSrc] = useState("")
+    const [cropped, setCropped] = useState("")
+    const cropper = useRef(null);
+    return (
+    <div>
+        <input
+            accept="image/*"
+            id="contained-button-file"
+            multiple
+            type="file"
+            style={{display: 'none'}}
+            onChange={(e): void => {
+                if (e && e.target && e.target.files) {
+                    return setImgSrc(URL.createObjectURL(e.target.files[0]))
+                }
+            }}
+        />
+      <label htmlFor="contained-button-file">
+        <Button variant="contained" color="primary" component="span">
+          Upload
+        </Button>
+      </label>
+      <Cropper
+          ref={cropper}
+          src={imgSrc}
+          style={{height:400, width: '100%'}}
+          aspectRatio={1}
+          guides={false}
+          crop={(): void => setCropped(cropper.current.getCroppedCanvas().toDataURL())}
+      />
+        <img alt="cropped sketch" src={cropped} />
+                <Button variant="contained" color="primary" component="span" onClick={(): void =>
+                cropper.current.getCroppedCanvas().toBlob(blob => fetch(api, {
+                    method: 'post',
+                    headers: {'Content-Type': ''},
+                    body: blob,
+                }).then(response => console.log(response)))}>
+            Submit
+        </Button>
+    </div>
+    )
+}
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div>
-            Here is some plain text
-        <Button>Hi</Button>
-            </div>
+      <CssBaseline />
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" >
+                    Sketchist
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <UploadImage />
     </ThemeProvider>
   );
-}
+};
 
 export default App;
